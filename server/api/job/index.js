d1df66f59a4947444ce8bb936def5f6a6b9bb0d0
Pawel Socha <psocha@gmail.com>
2015-01-30 17:02:55 -0500

    cheerio pulling in images and summary

--- server/api/job/index.js
+++ server/api/job/index.js
@@ -8,6 +8,7 @@ var router = express.Router();
 router.get('/', controller.index);
 router.get('/:id', controller.show);
 router.post('/', controller.create);
+router.post('/cheerio', controller.getCheerio);
 router.put('/:id', controller.update);
 router.patch('/:id', controller.update);
 router.delete('/:id', controller.destroy);
