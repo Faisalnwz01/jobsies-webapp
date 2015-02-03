722271cfcaf365dd44dfa1a9b99d1a1fac5b1044
Michael J. Abraham <mjabraham@gmail.com>
2015-02-03 18:14:30 -0500

    rstuff

--- client/app/rhome/rhome.controller.js
+++ client/app/rhome/rhome.controller.js
@@ -1,7 +1,7 @@
 'use strict';
 
 angular.module('jobsiesApp')
-  .controller('RhomeCtrl', function ($scope, User, $http, Auth) {
+  .controller('RhomeCtrl', function ($scope, User, $http, Auth, $mdSidenav, $log) {
     $scope.recruiter = {};
 
     $scope.user = User.get().$promise.then(function(user) {
@@ -41,13 +41,41 @@ angular.module('jobsiesApp')
    })
 
     $scope.userID = Auth.getCurrentUser()._id;
+      $scope.showjobs = false;
 
+
+
+
+    $scope.showJobPost = function() {
+      if ($scope.showjobs === false) {
+      $scope.showusers = false;
+      $scope.showjobs = true;
+      }
+      else {
+      $scope.showjobs = false;
+      }
+    }
+      
+    $scope.showusers = false;
+
+
+
+
+    $scope.showUsers = function() {
+      if ($scope.showusers === false) {
+      $scope.showjobs = false;
+      $scope.showusers = true;
+      }
+      else {
+      $scope.showusers = false;
+      }
+    }
     $scope.postJob = function(job) {
     console.log("step1")
     $scope.job = angular.copy(job);
     $scope.job.recruiter_id = $scope.userID;
     console.log($scope.job)
-    $scope.jobs.push($scope.job)
+    // $scope.jobs.push($scope.job)
       $http.post('/api/jobs/', $scope.job).success(function(jobs){
         console.log("JOB DONE", jobs);
         var jobs_id = jobs._id
@@ -55,6 +83,57 @@ angular.module('jobsiesApp')
           console.log(data);
       });
       });
+      $scope.formReset();
      }
+$scope.formReset = function() {
+    $scope.job.jobtitle = '';
+    $scope.job.formattedLocation = '';
+    $scope.job.company = '';
+    $scope.job.snippet = '';
+    $scope.job.qualifications = '';
+    $scope.showjobs = false;
+}
+
+$scope.discardForm = function() {
+  $scope.job.jobtitle = '';
+    $scope.job.formattedLocation = '';
+    $scope.job.company = '';
+    $scope.job.snippet = '';
+    $scope.job.qualifications = '';
+    $scope.showjobs = false;
+}
+
+  $scope.toggleLeft = function() {
+    $mdSidenav('left').toggle()
+                      .then(function(){
+                          $log.debug("toggle left is done");
+                      });
+  };
+  $scope.toggleRight = function() {
+    $mdSidenav('right').toggle()
+                        .then(function(){
+                          $log.debug("toggle RIGHT is done");
+                        });
+  };
+
+
+  })
+.controller('LeftCtrl', function($scope, $timeout, $mdSidenav, $log) {
+  $scope.close = function() {
+    $mdSidenav('left').close()
+                      .then(function(){
+                        $log.debug("close LEFT is done");
+                      });
+  };
+})
+.controller('RightCtrl', function($scope, $timeout, $mdSidenav, $log) {
+  $scope.close = function() {
+    $mdSidenav('right').close()
+                        .then(function(){
+                          $log.debug("close RIGHT is done");
+                        });
+  };
+
+
 
   })
